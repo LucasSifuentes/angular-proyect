0b9d576ca8538f386dcd6479e7526b5a47978272
Lucas Sufuentes <lucassifuentes23@gmail.com>
2024-11-01 00:54:56 -0300

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from "./components/skills/skills.component";
 import { ProyectsComponent } from './components/proyects/proyects.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AboutComponent, SkillsComponent, ProyectsComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, SkillsComponent, ProyectsComponent, FooterComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
